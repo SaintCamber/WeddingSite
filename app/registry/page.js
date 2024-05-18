111a451371c5165b67ef3a470cb1994bd0d554e2
Sarah <kretaksson@gmail.com>
2024-05-18 07:00:28 -0700

    3

--- app/registry/page.js
+++ app/registry/page.js
@@ -6,6 +6,9 @@ export default function Registry() {
         <div>
             <h1>Registry</h1>
             <p>Here you can find the registry for the wedding.</p>
+            <p>More information will be available soon.</p>
+            <p>Thank you for your patience.</p>
+            
         </div>
     )
 }
